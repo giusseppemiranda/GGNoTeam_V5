687005845238f4eb95c67d552cd2247843a8735f
giusseppemiranda <giovanni.miranda@pucp.edu.pe>
2021-11-14 14:28:37 -0500

    Se agrega Monitoreo de Ordenes

---
+++ VentanaPrincipal/MonitoreoOrdenes/frmMonitoreoOrdenes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GGNoTeam_V5.VentanaPrincipal.MonitoreoOrdenes
+{
+    public partial class frmMonitoreoOrdenes : Form
+    {
+        public frmMonitoreoOrdenes()
+        {
+            InitializeComponent();
+        }
+    }
+}
