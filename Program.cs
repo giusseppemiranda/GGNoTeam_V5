eb9b1bbfd454c86c6d8b2ab6f1adf55aabe20372
giusseppemiranda <giovanni.miranda@pucp.edu.pe>
2021-11-29 23:31:14 -0500

    Cambios mínimos

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
-using GGNoTeam_V5.Recursos;
-using GGNoTeam_V5.Recursos.PantallaDeCarga;
-using GGNoTeam_V5.Recursos.UserControls;
-using GGNoTeam_V5.VentanaPrincipal.MonitoreoOrdenes;
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GGNoTeam_V5
@@ -19,13 +10,10 @@ namespace GGNoTeam_V5
         /// </summary>
         [STAThread]
         static void Main()
-        {            
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
-            
-            //Global_LoadingForm.Show();
-
         }
     }
 }
--- VentanaPrincipal/Bandeja de Entrada/frmGmail.Designer.cs
+++ VentanaPrincipal/Bandeja de Entrada/frmGmail.Designer.cs
@@ -30,24 +30,24 @@ namespace GGNoTeam_V5.VentanaPrincipal.Bandeja_de_Entrada
         private void InitializeComponent()
         {
             this.panelMensaje = new System.Windows.Forms.Panel();
+            this.lblNumeroMensaje = new System.Windows.Forms.Label();
+            this.msgDate = new System.Windows.Forms.Label();
+            this.msgAsunto = new System.Windows.Forms.Label();
+            this.msgFrom = new System.Windows.Forms.Label();
+            this.lblCuerpoMSG = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblAsunto = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
             this.btnNext = new FontAwesome.Sharp.IconButton();
             this.btnBefore = new FontAwesome.Sharp.IconButton();
-            this.lblFrom = new System.Windows.Forms.Label();
-            this.lblAsunto = new System.Windows.Forms.Label();
-            this.lblFecha = new System.Windows.Forms.Label();
-            this.lblCuerpoMSG = new System.Windows.Forms.Label();
-            this.msgFrom = new System.Windows.Forms.Label();
-            this.msgAsunto = new System.Windows.Forms.Label();
-            this.msgDate = new System.Windows.Forms.Label();
-            this.msgCuerpo = new System.Windows.Forms.Label();
-            this.lblNumeroMensaje = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.panelMensaje.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMensaje
             // 
+            this.panelMensaje.Controls.Add(this.richTextBox1);
             this.panelMensaje.Controls.Add(this.lblNumeroMensaje);
-            this.panelMensaje.Controls.Add(this.msgCuerpo);
             this.panelMensaje.Controls.Add(this.msgDate);
             this.panelMensaje.Controls.Add(this.msgAsunto);
             this.panelMensaje.Controls.Add(this.msgFrom);
@@ -59,6 +59,97 @@ namespace GGNoTeam_V5.VentanaPrincipal.Bandeja_de_Entrada
             this.panelMensaje.Name = "panelMensaje";
             this.panelMensaje.Size = new System.Drawing.Size(881, 547);
             this.panelMensaje.TabIndex = 0;
+            // 
+            // lblNumeroMensaje
+            // 
+            this.lblNumeroMensaje.AutoSize = true;
+            this.lblNumeroMensaje.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroMensaje.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblNumeroMensaje.Location = new System.Drawing.Point(818, 123);
+            this.lblNumeroMensaje.Name = "lblNumeroMensaje";
+            this.lblNumeroMensaje.Size = new System.Drawing.Size(38, 17);
+            this.lblNumeroMensaje.TabIndex = 24;
+            this.lblNumeroMensaje.Text = "# / #";
+            // 
+            // msgDate
+            // 
+            this.msgDate.AutoSize = true;
+            this.msgDate.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.msgDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgDate.ForeColor = System.Drawing.Color.DimGray;
+            this.msgDate.Location = new System.Drawing.Point(178, 75);
+            this.msgDate.Name = "msgDate";
+            this.msgDate.Size = new System.Drawing.Size(112, 17);
+            this.msgDate.TabIndex = 22;
+            this.msgDate.Text = "-FECHA Y HORA-";
+            // 
+            // msgAsunto
+            // 
+            this.msgAsunto.AutoSize = true;
+            this.msgAsunto.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.msgAsunto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgAsunto.ForeColor = System.Drawing.Color.DimGray;
+            this.msgAsunto.Location = new System.Drawing.Point(178, 48);
+            this.msgAsunto.Name = "msgAsunto";
+            this.msgAsunto.Size = new System.Drawing.Size(71, 17);
+            this.msgAsunto.TabIndex = 21;
+            this.msgAsunto.Text = "-ASUNTO-";
+            // 
+            // msgFrom
+            // 
+            this.msgFrom.AutoSize = true;
+            this.msgFrom.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.msgFrom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgFrom.ForeColor = System.Drawing.Color.DimGray;
+            this.msgFrom.Location = new System.Drawing.Point(178, 18);
+            this.msgFrom.Name = "msgFrom";
+            this.msgFrom.Size = new System.Drawing.Size(55, 17);
+            this.msgFrom.TabIndex = 20;
+            this.msgFrom.Text = "-FROM-";
+            // 
+            // lblCuerpoMSG
+            // 
+            this.lblCuerpoMSG.AutoSize = true;
+            this.lblCuerpoMSG.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCuerpoMSG.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblCuerpoMSG.Location = new System.Drawing.Point(3, 105);
+            this.lblCuerpoMSG.Name = "lblCuerpoMSG";
+            this.lblCuerpoMSG.Size = new System.Drawing.Size(117, 17);
+            this.lblCuerpoMSG.TabIndex = 19;
+            this.lblCuerpoMSG.Text = "Mensaje recibido:";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblFecha.Location = new System.Drawing.Point(3, 75);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(99, 17);
+            this.lblFecha.TabIndex = 18;
+            this.lblFecha.Text = "Fecha enviada:";
+            // 
+            // lblAsunto
+            // 
+            this.lblAsunto.AutoSize = true;
+            this.lblAsunto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsunto.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblAsunto.Location = new System.Drawing.Point(3, 48);
+            this.lblAsunto.Name = "lblAsunto";
+            this.lblAsunto.Size = new System.Drawing.Size(56, 17);
+            this.lblAsunto.TabIndex = 17;
+            this.lblAsunto.Text = "Asunto:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblFrom.Location = new System.Drawing.Point(3, 18);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(142, 17);
+            this.lblFrom.TabIndex = 16;
+            this.lblFrom.Text = "Mensaje enviado por:";
             // 
             // btnNext
             // 
@@ -88,107 +179,13 @@ namespace GGNoTeam_V5.VentanaPrincipal.Bandeja_de_Entrada
             this.btnBefore.UseVisualStyleBackColor = true;
             this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
             // 
-            // lblFrom
+            // richTextBox1
             // 
-            this.lblFrom.AutoSize = true;
-            this.lblFrom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFrom.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblFrom.Location = new System.Drawing.Point(3, 18);
-            this.lblFrom.Name = "lblFrom";
-            this.lblFrom.Size = new System.Drawing.Size(142, 17);
-            this.lblFrom.TabIndex = 16;
-            this.lblFrom.Text = "Mensaje enviado por:";
-            // 
-            // lblAsunto
-            // 
-            this.lblAsunto.AutoSize = true;
-            this.lblAsunto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAsunto.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblAsunto.Location = new System.Drawing.Point(3, 48);
-            this.lblAsunto.Name = "lblAsunto";
-            this.lblAsunto.Size = new System.Drawing.Size(56, 17);
-            this.lblAsunto.TabIndex = 17;
-            this.lblAsunto.Text = "Asunto:";
-            // 
-            // lblFecha
-            // 
-            this.lblFecha.AutoSize = true;
-            this.lblFecha.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFecha.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblFecha.Location = new System.Drawing.Point(3, 75);
-            this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(99, 17);
-            this.lblFecha.TabIndex = 18;
-            this.lblFecha.Text = "Fecha enviada:";
-            // 
-            // lblCuerpoMSG
-            // 
-            this.lblCuerpoMSG.AutoSize = true;
-            this.lblCuerpoMSG.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCuerpoMSG.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblCuerpoMSG.Location = new System.Drawing.Point(3, 105);
-            this.lblCuerpoMSG.Name = "lblCuerpoMSG";
-            this.lblCuerpoMSG.Size = new System.Drawing.Size(117, 17);
-            this.lblCuerpoMSG.TabIndex = 19;
-            this.lblCuerpoMSG.Text = "Mensaje recibido:";
-            // 
-            // msgFrom
-            // 
-            this.msgFrom.AutoSize = true;
-            this.msgFrom.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.msgFrom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.msgFrom.ForeColor = System.Drawing.Color.DimGray;
-            this.msgFrom.Location = new System.Drawing.Point(178, 18);
-            this.msgFrom.Name = "msgFrom";
-            this.msgFrom.Size = new System.Drawing.Size(55, 17);
-            this.msgFrom.TabIndex = 20;
-            this.msgFrom.Text = "-FROM-";
-            // 
-            // msgAsunto
-            // 
-            this.msgAsunto.AutoSize = true;
-            this.msgAsunto.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.msgAsunto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.msgAsunto.ForeColor = System.Drawing.Color.DimGray;
-            this.msgAsunto.Location = new System.Drawing.Point(178, 48);
-            this.msgAsunto.Name = "msgAsunto";
-            this.msgAsunto.Size = new System.Drawing.Size(71, 17);
-            this.msgAsunto.TabIndex = 21;
-            this.msgAsunto.Text = "-ASUNTO-";
-            // 
-            // msgDate
-            // 
-            this.msgDate.AutoSize = true;
-            this.msgDate.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.msgDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.msgDate.ForeColor = System.Drawing.Color.DimGray;
-            this.msgDate.Location = new System.Drawing.Point(178, 75);
-            this.msgDate.Name = "msgDate";
-            this.msgDate.Size = new System.Drawing.Size(112, 17);
-            this.msgDate.TabIndex = 22;
-            this.msgDate.Text = "-FECHA Y HORA-";
-            // 
-            // msgCuerpo
-            // 
-            this.msgCuerpo.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.msgCuerpo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.msgCuerpo.ForeColor = System.Drawing.Color.DimGray;
-            this.msgCuerpo.Location = new System.Drawing.Point(3, 140);
-            this.msgCuerpo.Name = "msgCuerpo";
-            this.msgCuerpo.Size = new System.Drawing.Size(875, 398);
-            this.msgCuerpo.TabIndex = 23;
-            this.msgCuerpo.Text = "-MENSAJE RECIBIDO-";
-            // 
-            // lblNumeroMensaje
-            // 
-            this.lblNumeroMensaje.AutoSize = true;
-            this.lblNumeroMensaje.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumeroMensaje.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblNumeroMensaje.Location = new System.Drawing.Point(818, 123);
-            this.lblNumeroMensaje.Name = "lblNumeroMensaje";
-            this.lblNumeroMensaje.Size = new System.Drawing.Size(38, 17);
-            this.lblNumeroMensaje.TabIndex = 24;
-            this.lblNumeroMensaje.Text = "# / #";
+            this.richTextBox1.Location = new System.Drawing.Point(0, 143);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(881, 404);
+            this.richTextBox1.TabIndex = 25;
+            this.richTextBox1.Text = "";
             // 
             // frmGmail
             // 
@@ -215,11 +212,11 @@ namespace GGNoTeam_V5.VentanaPrincipal.Bandeja_de_Entrada
         private System.Windows.Forms.Label lblAsunto;
         private System.Windows.Forms.Label lblFrom;
         private System.Windows.Forms.Label lblFecha;
-        private System.Windows.Forms.Label msgCuerpo;
         private System.Windows.Forms.Label msgDate;
         private System.Windows.Forms.Label msgAsunto;
         private System.Windows.Forms.Label msgFrom;
         private System.Windows.Forms.Label lblCuerpoMSG;
         private System.Windows.Forms.Label lblNumeroMensaje;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
